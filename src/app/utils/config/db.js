bdbc64b80a52e39ec026eab87383e2e680dbc32b
Vishnu Kothakapu <vishnukothakapu27@gmail.com>
2025-03-08 13:39:03 +0530

    fix some bugs

--- src/app/utils/config/db.js
+++ src/app/utils/config/db.js
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
+
 let isConnected = false;
-const connectToDB=async()=>{
-    if (isConnected) return;
-try{
-    await mongoose.connect(process.env.MONGO_URI);
-    isConnected = true;
-    console.log("Connected to Database");
-}
-catch(err){
-    console.log(err);
-    throw new Error('Database connection failed');
-}
-}
+
+const connectToDB = async () => {
+    if (mongoose.connection.readyState >= 1) {
+        return;
+    }
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        isConnected = true;
+        console.log("Connected to Database");
+    } catch (err) {
+        console.error("Database connection failed:", err);
+        throw new Error("Database connection failed");
+    }
+};
+
 export default connectToDB;
